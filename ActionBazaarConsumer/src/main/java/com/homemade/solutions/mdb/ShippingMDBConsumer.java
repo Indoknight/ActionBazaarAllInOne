9cceac63d5b34386fe1aefb959540c5101201573
Naren <narendranath.chivukula@gmail.com>
2014-04-20 22:47:29 +0100

    Updated

--- ActionBazaarConsumer/src/main/java/com/homemade/solutions/mdb/ShippingMDBConsumer.java
+++ ActionBazaarConsumer/src/main/java/com/homemade/solutions/mdb/ShippingMDBConsumer.java
@@ -1,48 +1,77 @@
 package com.homemade.solutions.mdb;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import javax.sql.DataSource;
 
-import org.jboss.annotation.ejb.ResourceAdapter;
-
-
-@ResourceAdapter("activemq-rar-5.8.0.rar")
-@MessageDriven(
-		name="ShippingMDB",
-		activationConfig={
-			@ActivationConfigProperty(propertyName="destinationType", propertyValue="javax.jms.Queue"),
-			@ActivationConfigProperty(propertyName="destination", propertyValue="hornetq/queue/ActionBazaarShippingQueue"),
-			@ActivationConfigProperty(propertyName="acknowledgeMode", propertyValue="Auto-acknowledge")
-		}	
-)
+//@ResourceAdapter("activemq-rar-5.8.0.rar")
+@MessageDriven(name = "ShippingMDB", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/queue/ActionBazaarShippingQueue"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") }
+		//,messageListenerInterface = MessageListener.class
+		)
 public class ShippingMDBConsumer implements MessageListener {
 	
+	private Connection connection;
+
+	@Resource
+	private MessageDrivenContext mdc;
 	
+	
+	@Resource(name="java:jboss/datasources/OjdbcDS")
+	//@Resource
+	private DataSource ds;
+
 	@PostConstruct
-	public void initialize(){
+	public void initialize() {
 		System.out.println("Initializing************************");
-	}
-	
-	
-	public void onMessage(Message message){
+		System.out.println("Message Driven Context: " + mdc);
+		System.out.println("DataSource: " + ds);
 		try{
+			connection = ds.getConnection();
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
+		
+	}
+
+	public void onMessage(Message message) {
+		if (mdc != null) {
+
+			System.out.println("Caller Principal :" + mdc.getCallerPrincipal());
+			System.out.println("Rollback only :" + mdc.getRollbackOnly());
+			// You get IllegalStateException when used with default Container Managed Transactions
+			//System.out.println("User Transaction " + mdc.getUserTransaction());
+		}
+		try {
 			TextMessage txtMsg = (TextMessage) message;
-			
-			System.out.println("Got a message \n"+ txtMsg.getText());
-		}catch(Throwable thr){
+
+			System.out.println("Got a message \n" + txtMsg.getText());
+		} catch (Throwable thr) {
 			thr.printStackTrace();
 		}
 	}
-	
-	
+
 	@PreDestroy
-	public void cleanUp(){
+	public void cleanUp() {
 		System.out.println("Cleaning up************************");
+		try{
+			connection.close();
+			connection = null;
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
 	}
-	
+
 }
